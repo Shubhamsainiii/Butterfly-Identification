faedcc5f4640055b0429c878af5baf2c17cea443
saini3901 <201951149@iiitvadodara.ac.in>
2021-10-09 10:34:25 +0530

    Added crop

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:tflite/tflite.dart';
 
+import 'package:image_cropper/image_cropper.dart';
+
 void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
@@ -43,36 +45,62 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("BUTTERFLY"),
+        title: Text("BUTTERFLY-DETECTION"),
       ),
       body: _isLoading ? Container(
         alignment: Alignment.center,
-        child: CircularProgressIndicator(),
+        child: Image.asset("assets/monarch-butterfly-in-childs-hands.jpg"),
       ) : Container(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            _image == null ? Container() : Image.file(_image),
+            _image == null ? Container(
+              alignment: Alignment.center,
+              child: Center(child: Image.asset("assets/monarch-butterfly-in-childs-hands.jpg")),
+
+              height: 400,
+              width: 400,
+              // child: CircularProgressIndicator(),
+            )  : Container(
+              height: 400,
+              width: 400,
+              child: Image.file(_image),
+            ),
             SizedBox(height: 16,),
-            _output == null ? Text(""): Text(
+            _output == null ? Text("UPLOAD IMAGE"): Text(
              "${_output[0]["label"]}"
             )
           ],
 
         ),
 
-
+          // Image.file(_image)
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          chooseImage();
+      floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: (){
+              chooseImage();
 
-        },
-        child: Icon(
-          Icons.image
-        ),
+            },
+            child: Icon(
+              Icons.image
+            ),
 
+          ),
+          FloatingActionButton(
+            onPressed: (){
+              pickImage();
+
+            },
+            child: Icon(
+                Icons.camera_alt
+            ),
+
+          ),
+        ],
       ),
     );
   }
@@ -88,20 +116,81 @@ class _HomePageState extends State<HomePage> {
   //
   // }
 
+
+
+  Future pickImage() async {
+    var image = await ImagePicker().getImage(source: ImageSource.camera);
+    var image1 = await ImageCropper.cropImage(
+      sourcePath: image.path,
+      aspectRatio: CropAspectRatio(ratioX: 1, ratioY: 1),
+      compressQuality: 100,
+      maxHeight: 700,
+      maxWidth: 700,
+      compressFormat: ImageCompressFormat.jpg,
+      androidUiSettings: AndroidUiSettings(
+        toolbarColor: Colors.white,
+        toolbarTitle: "genie cropper",
+      ),
+    );
+    //print("cropper ${val.runtimeType}");
+    //capturedImageFile(val.path);
+
+
+
+
+    if (image1 == null) {
+      return null;
+    }
+    setState(() {
+      _image = File(image1.path);
+    });
+    runModelOnImage(File(image1.path));
+  }
+
+
+
+
+
   Future chooseImage() async{
     // var image = await ImagePicker().pickImage(source: ImageSource.gallery);
     var image = await ImagePicker().pickImage(source: ImageSource.gallery);
-    if(image == null){
+    var image1 = await ImageCropper.cropImage(
+      sourcePath: image.path,
+      aspectRatio: CropAspectRatio(ratioX: 1, ratioY: 1),
+      compressQuality: 100,
+      maxHeight: 700,
+      maxWidth: 700,
+      compressFormat: ImageCompressFormat.jpg,
+      androidUiSettings: AndroidUiSettings(
+        toolbarColor: Colors.white,
+        toolbarTitle: "genie cropper",
+      ),
+    );
+    //print("cropper ${val.runtimeType}");
+    //capturedImageFile(val.path);
 
-      return null;}
-      setState(() {
-        print ("shubham");
-        _isLoading = true;
-        // _image = image as File;
-        _image = File(image.path);
-      });
-      runModelOnImage(File(image.path));
+
+
+
+    if (image1 == null) {
+      return null;
+    }
+    setState(() {
+      _image = File(image1.path);
+    });
+    runModelOnImage(File(image1.path));
   }
+    // if(image == null){
+
+  //     return null;}
+  //     setState(() {
+  //       print ("shubham");
+  //       _isLoading = true;
+  //       // _image = image as File;
+  //       _image = File(image.path);
+  //     });
+  //     runModelOnImage(File(image.path));
+  // }
 
   runModelOnImage(File image) async{
     var output = await Tflite.runModelOnImage(
